a09dd3b10392ba00fddeac87801a23c3abd8d6a0
Vitaly Andrianov <vitaly.andr@gmail.com>
2024-08-07 16:28:21 +0300

    Refactor: removed IF ladder

--- app.rb
+++ app.rb
@@ -14,19 +14,38 @@ class TimeApp
   private
   def handle_time_request(request)
     format_param = request.params['format']
-    if format_param
-      formats = format_param.split(',')
-      unknown_formats = formats - VALID_FORMATS
-      if unknown_formats.any?
-        [400, { 'Content-Type' => 'text/plain' }, ["Unknown time format [#{unknown_formats.join(', ')}]"]]
-      else
-        formatted_time = formats.map { |format| format_time(format) }.join('-')
-        [200, { 'Content-Type' => 'text/plain' }, [formatted_time]]
-      end
-    else
-      [400, { 'Content-Type' => 'text/plain' }, ['Format parameter is required']]
-    end
+    return respond_with_missing_format_error unless format_param
+
+    formats = format_param.split(',')
+    unknown_formats = formats - VALID_FORMATS
+    return respond_with_error(unknown_formats) if unknown_formats.any?
+
+    respond_with_formatted_time(formats)
   end
+
+  def respond_with_missing_format_error
+    [400, { 'Content-Type' => 'text/plain' }, ['Format parameter is required']]
+  end
+
+  def respond_with_error(unknown_formats)
+    [400, { 'Content-Type' => 'text/plain' }, ["Unknown time format [#{unknown_formats.join(', ')}]"]]
+  end
+
+  def respond_with_formatted_time(formats)
+    formatted_time = formats.map { |format| format_time(format) }.join('-')
+    [200, { 'Content-Type' => 'text/plain' }, [formatted_time]]
+  end
+
+    # if unknown_formats.any?
+    #     [400, { 'Content-Type' => 'text/plain' }, ["Unknown time format [#{unknown_formats.join(', ')}]"]]
+    #   else
+    #     formatted_time = formats.map { |format| format_time(format) }.join('-')
+    #     [200, { 'Content-Type' => 'text/plain' }, [formatted_time]]
+    #   end
+    # else
+    #   [400, { 'Content-Type' => 'text/plain' }, ['Format parameter is required']]
+    # end
+  # end
 
   def format_time(format)
     current_time = Time.now
